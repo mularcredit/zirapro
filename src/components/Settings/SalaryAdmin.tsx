f0b2cfa4c454f574fac14e1e11ba80a0e39c4fa3
baliech <duncanian33@gmail.com>
2025-11-15 06:36:44 +0300

    first commit

--- src/components/Settings/SalaryAdmin.tsx
+++ src/components/Settings/SalaryAdmin.tsx
@@ -4212,14 +4212,15 @@ const processMpesaPayment = async (employeeNumber: string, amount: number, fullN
                   onClick={() => setShowExportModal(true)}
                   className="flex items-center gap-2 px-3 py-2 border border-gray-300 rounded-md text-xs font-medium text-gray-700 bg-white hover:bg-gray-50"
                 >
-                  <Download className="w-4 h-4" />
+                  <img src='logo (2).png' className='w-5'></img>
                   Export
                 </button>
                 <button
                   onClick={() => setShowImportModal(true)}
                   className="flex items-center gap-2 px-3 py-2 border border-gray-300 rounded-md text-xs font-medium text-gray-700 bg-white hover:bg-gray-50"
                 >
-                  <Upload className="w-4 h-4" />
+                  
+                  <img src='stats.png' className='w-5'></img>
                   Import
                 </button>
               </div>
